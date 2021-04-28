a8886b0f3e24244e1b18a6148abfaf6cc1233a7a
PlayMa256 <matheus.g.silva@hotmail.com>
2021-04-28 11:51:12 -0300

    chore: rename app

--- ios/CryptoPorfolio/AppDelegate.m
+++ ios/CryptoPorfolio/AppDelegate.m
@@ -33,7 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"AwesomeTSProject"
+                                                   moduleName:@"CryptoPorfolio"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
--- ios/CryptoPorfolioTests/CryptoPorfolioTests.m
+++ ios/CryptoPorfolioTests/CryptoPorfolioTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeTSProjectTests : XCTestCase
+@interface CryptoPorfolioTests : XCTestCase
 
 @end
 
-@implementation AwesomeTSProjectTests
+@implementation CryptoPorfolioTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
