5d3d06a96dee2993808700df4e7e028ebf905441
PlayMa256 <matheus.g.silva@hotmail.com>
2021-04-28 12:23:35 -0300

    feat(navigator): add navigator boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import {Home} from './routes';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
@@ -16,7 +20,17 @@ const App = () => {
           flex: 1,
         }}>
         <StatusBar />
-        <Home />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
       <SafeAreaView
         style={{
